b3f33e47db3d15064996f0a5b8f5d766cc37352b
ziad <ziadoLearningGIT@mindyourownbusniss.com>
2024-11-06 00:08:16 -0800

    replace buttons with slider, make inputs controlled by react state

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ function App() {
   const date = new Date();
   date.setDate(date.getDate() + counter);
 
+  
   const dateString = date.toDateString();
+  
+  function reset(){
+setCounter(0);
+setStep(1);    
+  }
+  
   return (
 
     <>
 
       <div>
 
+        <input type='range' onChange={(e)=>setStep(Number(e.target.value))} value={step} Min={0}/> {step} <br/>
         <button onClick={() => setCounter(counter - step)}>-</button>
-        counter: <strong >{counter}</strong>
+        <input type='number' onChange={(e)=>setCounter(Number(e.target.value))} value={counter}/>
+        
+        {/* counter: <strong >{counter}</strong> */}
         <button onClick={() => setCounter(counter + step)} >+</button>
 
       </div>
       <div>
-        <button onClick={() => setStep(step>1?step-1: step)}>-</button>  
-        step: <strong >{step}</strong>
-        <button onClick={() => setStep(step + 1)} >+</button>
+        {/* <button onClick={() => setStep(step>1?step-1: step)}>-</button>   */}
+        {/* step: <strong >{step}</strong> */}
+        {/* <button onClick={() => setStep(step + 1)} >+</button> */}
 
       </div>
 
@@ -35,6 +45,11 @@ function App() {
 
       
       }</div>
+
+
+
+      {(counter!==0 || step!==1)&&     <button type='reset' onClick={reset}> reset</button>
+ }
     </>
 
   );
